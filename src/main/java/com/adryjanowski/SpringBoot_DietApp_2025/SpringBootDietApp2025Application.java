92b6640f41495ac488a5969822e3fa37d07830d4
adryjanowski <adryjanowski@gmail.com>
2025-09-16 22:27:11 +0200

    first commit

---
+++ src/main/java/com/adryjanowski/SpringBoot_DietApp_2025/SpringBootDietApp2025Application.java
@@ -0,0 +1,13 @@
+package com.adryjanowski.SpringBoot_DietApp_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDietApp2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDietApp2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/adryjanowski/SpringBoot_DietApp_2025/SpringBootDietApp2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adryjanowski.SpringBoot_DietApp_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDietApp2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
